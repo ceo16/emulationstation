f7357c0277d2beb3bfa98ef05ef8fe85ed434b7b
ceo16 <ceo16@libero.it>
2025-03-19 21:45:28 +0100

    Update main.cpp

--- es-app/src/main.cpp
+++ es-app/src/main.cpp
@@ -46,7 +46,7 @@
  #include "EpicGamesStore/EpicGamesStoreAPI.h" // Include our Epic Games Store API class
  #include "FileData.h" // Include EmulationStation's FileData class
  #include <vector>
-#include <../../es-core/src/Windows.h>
+#include <Windows.h>
 
 #ifdef WIN32
 #include <Windows.h>
