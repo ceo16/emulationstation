177f43558e5d73fe0cd840c4527bba5fb09376c2
ceo16 <ceo16@libero.it>
2025-03-25 20:43:17 +0100

    Create URLEncoding.h

---
+++ es-app/src/URLEncoding.h
@@ -0,0 +1,8 @@
+#ifndef EMULATIONSTATION_URLENCODING_H
+#define EMULATIONSTATION_URLENCODING_H
+
+#include <string>
+
+std::string urlEncode(const std::string& value);
+
+#endif // EMULATIONSTATION_URLENCODING_H
