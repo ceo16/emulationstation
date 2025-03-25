673a674c0b6f756f44ba7c284279a00de853a887
ceo16 <ceo16@libero.it>
2025-03-25 22:53:49 +0100

    Update Win32ApiSystem.h

--- es-app/src/Win32ApiSystem.h
+++ es-app/src/Win32ApiSystem.h
@@ -5,63 +5,63 @@
 
 class Win32ApiSystem : public ApiSystem
 {
-public:	
-	Win32ApiSystem();
-	
-	virtual void deinit();
+public:
+    Win32ApiSystem();
 
-	bool isScriptingSupported(ScriptId script) override;
+    virtual void deinit();
 
-	std::vector<std::string> getSystemInformations() override;
-	std::vector<std::string> getAvailableStorageDevices() override;
+    bool isScriptingSupported(ScriptId script) override;
 
-	unsigned long getFreeSpaceGB(std::string mountpoint) override;
-	std::string getFreeSpaceInfo(const std::string dir);
-	std::string getFreeSpaceUserInfo() override;
-	std::string getFreeSpaceSystemInfo() override;	
-	std::vector<std::string> getVideoModes(const std::string output = "") override;
+    std::vector<std::string> getSystemInformations() override;
+    std::vector<std::string> getAvailableStorageDevices() override;
 
-	void setReadyFlag(bool ready = true) override;
-	bool isReadyFlagSet() override;
+    unsigned long getFreeSpaceGB(std::string mountpoint) override;
+    std::string getFreeSpaceInfo(const std::string dir);
+    std::string getFreeSpaceUserInfo() override;
+    std::string getFreeSpaceSystemInfo() override;
+    std::vector<std::string> getVideoModes(const std::string output = "") override;
 
-	// Bezels
-	virtual std::vector<BatoceraBezel> getBatoceraBezelsList();
-	virtual std::pair<std::string, int> installBatoceraBezel(std::string bezelsystem, const std::function<void(const std::string)>& func = nullptr);
-	virtual std::pair<std::string, int> uninstallBatoceraBezel(std::string bezelsystem, const std::function<void(const std::string)>& func = nullptr);
+    void setReadyFlag(bool ready = true) override;
+    bool isReadyFlagSet() override;
 
-	// Updates
-	std::pair<std::string, int> updateSystem(const std::function<void(const std::string)>& func) override;
-	bool canUpdate(std::vector<std::string>& output) override;
+    // Bezels
+    virtual std::vector<BatoceraBezel> getBatoceraBezelsList();
+    virtual std::pair<std::string, int> installBatoceraBezel(std::string bezelsystem, const std::function<void(const std::string)>& func = nullptr);
+    virtual std::pair<std::string, int> uninstallBatoceraBezel(std::string bezelsystem, const std::function<void(const std::string)>& func = nullptr);
 
-	bool ping() override;
+    // Updates
+    std::pair<std::string, int> updateSystem(const std::function<void(const std::string)>& func) override;
+    bool canUpdate(std::vector<std::string>& output) override;
 
-	bool launchKodi(Window *window) override;	
+    bool ping() override;
 
-	std::vector<std::string> getShaderList(const std::string& systemName, const std::string& emulator, const std::string& core) override;
+    bool launchKodi(Window *window) override;
 
-	virtual std::string getSevenZipCommand() override;
-	virtual std::string getHostsName() override;
+    std::vector<std::string> getShaderList(const std::string& systemName, const std::string& emulator, const std::string& core) override;
 
-	bool canSuspend();
-	virtual void suspend() override;
+    virtual std::string getSevenZipCommand() override;
+    virtual std::string getHostsName() override;
 
-	virtual bool isPlaneMode() override;
-	virtual bool setPlaneMode(bool enable) override;
-        virtual bool openUrl(const std::string& url) override; // Add this line
+    bool canSuspend();
+    virtual void suspend() override;
+
+    virtual bool isPlaneMode() override;
+    virtual bool setPlaneMode(bool enable) override;
+    virtual bool openUrl(const std::string& url) override; // Add this line
 
 protected:
 
-	bool executeScript(const std::string command) override;
-	std::pair<std::string, int> executeScript(const std::string command, const std::function<void(const std::string)>& func) override;
-	std::vector<std::string> executeEnumerationScript(const std::string command) override;
+    bool executeScript(const std::string command) override;
+    std::pair<std::string, int> executeScript(const std::string command, const std::function<void(const std::string)>& func) override;
+    std::vector<std::string> executeEnumerationScript(const std::string command) override;
 
 private:
-	void updateEmulatorLauncher(const std::function<void(const std::string)>& func);
-	void installEmulationStationZip(const std::string& zipFile);
+    void updateEmulatorLauncher(const std::function<void(const std::string)>& func);
+    void installEmulationStationZip(const std::string& zipFile);
 
-	int executeCMD(const char* lpCommandLine, std::string& output, const char* lpCurrentDirectory = nullptr, const std::function<void(const std::string)>& func = nullptr);
+    int executeCMD(const char* lpCommandLine, std::string& output, const char* lpCurrentDirectory = nullptr, const std::function<void(const std::string)>& func = nullptr);
 
-	void* m_hJob;
+    void* m_hJob;
 };
 
 
