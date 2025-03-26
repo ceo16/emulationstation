89b24cfb35a66889e135e5c80ae12d90f870e443
ceo16 <ceo16@libero.it>
2025-03-26 14:46:02 +0100

    Update WebViewGuiComponent.h

--- es-app/src/WebView/WebViewGuiComponent.h
+++ es-app/src/WebView/WebViewGuiComponent.h
@@ -1,1 +1,29 @@
+#ifndef EMULATIONSTATION_WEBVIEWGUICOMPONENT_H
+#define EMULATIONSTATION_WEBVIEWGUICOMPONENT_H
 
+#include "GuiComponent.h"
+#include <string>
+#include <functional>
+
+class WebViewGuiComponent : public GuiComponent {
+public:
+    WebViewGuiComponent(Window* window);
+    ~WebViewGuiComponent();
+
+    void loadUrl(const std::string& url);
+    void setNavigationCallback(std::function<void(const std::string&)> callback);
+    void setCloseCallback(std::function<void()> closeCallback);
+    std::string GetCurrentUrl(); // to get current URL
+
+private:
+    //  ... (Membri per gestire la libreria di visualizzazione web)
+    //  Esempio:
+    //  QtWebEngineView* webView; // Se si usa Qt WebEngine
+
+    std::function<void(const std::string&)> mNavigationCallback;
+    std::function<void()> mCloseCallback;
+    void handleNavigation(const std::string& url);
+    void handleClose();
+};
+
+#endif // EMULATIONSTATION_WEBVIEWGUICOMPONENT_H
