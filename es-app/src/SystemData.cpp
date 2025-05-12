c662d2d63757dd6742db8fa1b0dc01b919504f10
ceo16 <ceo16@libero.it>
2025-05-12 15:00:48 +0200

    Update SystemData.cpp

--- es-app/src/SystemData.cpp
+++ es-app/src/SystemData.cpp
@@ -1053,7 +1053,7 @@ void SystemData::loadAdditionnalConfig(pugi::xml_node& srcSystems)
   std::string fullname = system.child("fullname").text().get();
  
   if (window != NULL)
-  window->renderSplashScreen(fullname, systemCount == 0 ? 0 : (float)currentSystem / (float)(systemCount + 1));
+ window->renderSplashScreen(fullname, systemCount == 0 ? 0 : (float)currentSystem / (float)(systemCount + 2));
  
   std::string nm = system.child("name").text().get();
  
@@ -1074,7 +1074,7 @@ void SystemData::loadAdditionnalConfig(pugi::xml_node& srcSystems)
   pThreadPool->wait([window, &processedSystem, systemCount, &systemsNames] {
   int px = processedSystem - 1;
   if (px >= 0 && px < systemsNames.size())
-  window->renderSplashScreen(systemsNames.at(px), (float)px / (float)(systemCount + 1));
+  window->renderSplashScreen(systemsNames.at(px), (float)px / (float)(systemCount + 2));
   }, 50);
   } else
   pThreadPool->wait();
@@ -1089,10 +1089,10 @@ void SystemData::loadAdditionnalConfig(pugi::xml_node& srcSystems)
   delete pThreadPool;
  
   if (window != NULL)
-  window->renderSplashScreen(_("Collections"), systemCount == 0 ? 0 : currentSystem / systemCount);
+  window->renderSplashScreen(_("Collections"), systemCount == 0 ? 0 : currentSystem / (float)(systemCount + 1));
   } else {
   if (window != NULL)
-  window->renderSplashScreen(_("Collections"), systemCount == 0 ? 0 : currentSystem / systemCount);
+  window->renderSplashScreen(_("Collections"), systemCount == 0 ? 0 : currentSystem / (float)(systemCount + 1));
  
   CollectionSystemManager::get()->loadCollectionSystems();
   }
@@ -1659,7 +1659,7 @@ SystemData* SystemData::loadSystem(pugi::xml_node system, bool fullMode)
 	// platform id list
 	std::string platformList = system.child("platform").text().get();
 	std::vector<std::string> platformStrs = readList(platformList);
-	std::vector<PlatformIds::PlatformId> platformIds;
+	std::set<PlatformIds::PlatformId> platformIds;
 	for (auto it = platformStrs.cbegin(); it != platformStrs.cend(); it++)
 	{
 		const char* str = it->c_str();
@@ -1671,16 +1671,16 @@ SystemData* SystemData::loadSystem(pugi::xml_node system, bool fullMode)
 			platformIds.clear();
 
 			if (md.name == "imageviewer")
-				platformIds.push_back(PlatformIds::IMAGEVIEWER);
+				platformIds.insert(PlatformIds::IMAGEVIEWER);
 			else
-				platformIds.push_back(platformId);
+				platformIds.insert(platformId);
 
 			break;
 		}
 
 		// if there appears to be an actual platform ID supplied but it didn't match the list, warn
 		if (platformId != PlatformIds::PLATFORM_UNKNOWN)
-			platformIds.push_back(platformId);
+			platformIds.insert(platformId);
 		else if (str != NULL && str[0] != '\0' && platformId == PlatformIds::PLATFORM_UNKNOWN)
 			LOG(LogWarning) << "  Unknown platform for system \"" << md.name << "\" (platform \"" << str << "\" from list \"" << platformList << "\")";
 	}
@@ -2071,19 +2071,28 @@ GameCountInfo* SystemData::getGameCountInfo()
 
 			if (playCount > mostPlayCount)
 			{
-				mGameCountInfo->mostPlayed = game->getName();
+				mostCountPlayed = game->getName();
 				mostPlayCount = playCount;
 			}
 		}
 
 		long seconds = atol(game->getMetadata(MetaDataId::GameTime).c_str());
 		if (seconds > 0)
+			{
 			mGameCountInfo->playTime += seconds;
-
+if (seconds > gameTime)
+			{
+				mGameCountInfo->mostPlayed = game->getName();
+				gameTime = seconds;
+			}
+		}
 		auto lastPlayed = game->getMetadata(MetaDataId::LastPlayed);
 		if (!lastPlayed.empty() && lastPlayed > mGameCountInfo->lastPlayedDate)
 			mGameCountInfo->lastPlayedDate = lastPlayed;
 	}
+
+	if (mGameCountInfo->mostPlayed.empty() && !mostCountPlayed.empty())
+		mGameCountInfo->mostPlayed = mostCountPlayed;
 
 	return mGameCountInfo;
 	/*
